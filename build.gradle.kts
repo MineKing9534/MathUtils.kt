4ce1eedca5cc3d5b6378a7c1bfb6a8b9f62cfe92
MineKing <mineking9534@gmail.com>
2024-04-11 15:42:52 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import java.io.ByteArrayOutputStream
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    `maven-publish`
+}
+
+group = "de.mineking"
+version = "1.0.0"
+
+var commit = extractCommit()
+var release = System.getenv("RELEASE") == "true"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.mineking.dev/releases") }
+}
+
+dependencies {
+    implementation("de.mineking:MathUtils:1.0.0")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.mineking.dev/" + (if(release) "releases" else "snapshots"))
+
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "de.mineking"
+            artifactId = "MathUtils.kt"
+            version = if (release) this.version else commit
+
+            from(components["java"])
+        }
+    }
+}
+
+
+fun extractCommit(): String {
+    val os = ByteArrayOutputStream()
+    project.exec {
+        commandLine = "git rev-parse --verify --short HEAD".split(" ")
+        standardOutput = os
+    }
+    return String(os.toByteArray()).trim()
+}
---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,35 @@
+import de.mineking.math.node.Fraction
+import de.mineking.math.node.Node
+import de.mineking.math.node.Product
+import de.mineking.math.node.Sum
+import org.jetbrains.annotations.NotNull
+
+@NotNull
+operator fun Node.unaryMinus(): Node {
+    return negate()
+}
+
+@NotNull
+operator fun Node.plus(@NotNull node: Node): Node {
+    return Sum.create(this, node)
+}
+
+@NotNull
+operator fun Node.minus(@NotNull node: Node): Node {
+    return Sum.create(this, - node)
+}
+
+@NotNull
+operator fun Node.times(@NotNull node: Node): Node {
+    return Product.create(this, node)
+}
+
+@NotNull
+operator fun Node.div(@NotNull node: Node): Node {
+    return Fraction.create(this, node)
+}
+
+@NotNull
+operator fun Node.invoke(): Node {
+    return simplify()
+}
